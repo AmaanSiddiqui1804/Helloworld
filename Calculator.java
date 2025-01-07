ec4451b12ac1821aeca7e2c32fd847aacadf520e
AmaanSiddiqui1804 <amaansiddiqui1804@gmail.com>
2025-01-07 11:19:16 +0530

    Added calculator program consisting of only addition functionality to addition branch

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator{
+   public static void main(String [] args){ 
+      
+     int a = 6;
+     int b = 16;
+     int sum = a+b;
+     System.out.println("the sum of 6 and 16 is: " + sum);
+   }
+}
