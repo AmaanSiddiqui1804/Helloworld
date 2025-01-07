3e4898cb53d87c0b763edbded7661e28366c7656
AmaanSiddiqui1804 <amaansiddiqui1804@gmail.com>
2025-01-07 11:27:34 +0530

    Subtraction functionality added to Calculator in subtraction branch

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,34 @@
+import java.util.*;
 public class Calculator{
    public static void main(String [] args){ 
+   Scanner sc = new Scanner(System.in);
+   System.out.print("Enter first number: ");
+   int a = sc.nextInt();
+   System.out.println();
+   System.out.print("Enter second number: "); 
+   int b = sc.nextInt();
+   System.out.println("Enter name of Operation:" );
+   System.out.println("add || subtract ");
+   String op = sc.next();
+   
+   int sum, dif ;
+
+   if ("add".equalsIgnoreCase(op)) {
       
-     int a = 6;
-     int b = 16;
-     int sum = a+b;
-     System.out.println("the sum of 6 and 16 is: " + sum);
+         sum = a+b;
+         System.out.println("Addition of the given numbers is: " + sum);
+   }
+
+   else if ("subtract".equalsIgnoreCase(op)) {
+      
+         dif = a-b;
+         System.out.println("Difference of the given numbers is: " + dif);
+   }
+   
+   
+   else{
+        System.out.println("Error! Try again");
+   }
+
    }
 }
